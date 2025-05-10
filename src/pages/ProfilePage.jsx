adad51ad9eb6f57d991da93c00778d4b3fe513a3
yarkob <yaroslav.klark.bondarenko@gmail.com>
2025-05-10 14:29:18 +0300

    add ability to change email

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,42 +1,104 @@
 import s from './ProfilePage.module.scss';
 import React, {useEffect, useRef, useState} from "react";
 import {profileService} from "../services/profileService";
+import {validateEmail} from "../utils/validateEmail";
+import {validatePassword} from "../utils/validatePassword";
 
 export const ProfilePage = ({ user }) => {
-  const [isInput, setIsInput] = useState(false);
+  const [isNameInput, setIsNameInput] = useState(false);
+  const [isEmailInput, setIsEmailInput] = useState(false);
   const [name, setName] = useState(user.name);
+  const [newEmail, setNewEmail] = useState(user.email);
+  const [password, setPassword] = useState('');
   const [error, setError] = useState(null);
   const inputRef = useRef(null);
+  const emailRef = useRef(null);
 
   const nameHandler = () => {
-    setIsInput((prev) => !prev);
+    setIsNameInput(true);
+    if (!isNameInput) {
+      setError(null);
+    }
+  }
+
+  const emailHandler = () => {
+    setIsEmailInput(true);
+    if (!isEmailInput) {
+      setError(null);
+    }
   }
 
   const nameChangeHandler = (event) => {
     setName(event.target.value);
   }
 
-  const submitHandler = (event) => {
+  const nameSubmitHandler = (event) => {
     event.preventDefault();
 
     setName(event.target.name.value);
-    setIsInput(false);
-    profileService.changeName({name, email: user.email}).catch((error) => {
+    setIsNameInput(false);
+    profileService.changeName({name, email: user.email }).catch((error) => {
       setError(error.message);
     })
   }
 
+  const emailChangeHandler = (event) => {
+    setNewEmail(event.target.value);
+  }
+
+  const emailSubmitHandler = (event) => {
+    event.preventDefault();
+
+    setPassword(event.target.password.value);
+    setNewEmail(event.target.newEmail.value);
+    setIsEmailInput(false);
+    console.log({ oldEmail: user.email, newEmail, password });
+    profileService.changeEmail({ oldEmail: user.email, newEmail, password }).catch((error) => {
+      console.log(error.response.data);
+      setError(error.response?.data?.message);
+    })
+  }
+
+  const passwordChangeHandler = (event) => {
+    setPassword(event.target.value);
+  }
+
   useEffect(() => {
-    if (isInput && inputRef.current) {
+    if (isNameInput && inputRef.current) {
       inputRef.current.focus();
     }
-  }, [isInput])
+  }, [isNameInput])
+
+  useEffect(() => {
+    if (isEmailInput && emailRef.current) {
+      emailRef.current.focus();
+    }
+  }, [isEmailInput])
 
   return (
     <div className={s.container}>
       <div className={s.avatar}/>
       <div onDoubleClick={nameHandler}>
-        {isInput ? <form onSubmit={submitHandler}><input name="name" onChange={nameChangeHandler} ref={inputRef} value={name} /></form> : <h1 className={s.title}>{name}</h1>}
+        {isNameInput ?
+          <form onSubmit={nameSubmitHandler} className={s.form}>
+            <input
+              name="name"
+              onChange={nameChangeHandler}
+              ref={inputRef}
+              value={name}
+              className={s.input}
+            />
+            <button type='submit'>Submit</button>
+          </form> : <h1 className={s.title}>{name}</h1>}
+      </div>
+      <div onDoubleClick={emailHandler}>
+        {isEmailInput ?
+          <form onSubmit={emailSubmitHandler} className={s.form}>
+            <input className={s.input} name="newEmail" onChange={emailChangeHandler} ref={emailRef} value={newEmail}/>
+            <input className={s.input} name="password" onChange={passwordChangeHandler} value={password}
+                   type="password"/>
+            <button type='submit'>Submit</button>
+          </form> : <p>{newEmail}</p>}
       </div>
       {error && <p className="notification is-danger is-light">{error}</p>}
     </div>
--- src/services/profileService.js
+++ src/services/profileService.js
@@ -1,7 +1,11 @@
 import {profileClient} from "../http/profileClient";
 
 export const changeName = (user) => {
-  return profileClient.post('/profile', {name: user.name, email: user.email});
+  return profileClient.post('/profile/name', {name: user.name, email: user.email});
 }
 
-export const profileService = { changeName };
+export const changeEmail = (user) => {
+  return profileClient.post('/profile/email', {newEmail: user.newEmail, oldEmail: user.oldEmail, password: user.password});
+}
+
+export const profileService = { changeName, changeEmail };
